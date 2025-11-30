03e1277904b25510c1c52fbe4737213cfd34e42f
Robin Ray-Chaudhuri <robinray-chaudhuri@robins-air.home>
2025-11-30 12:22:01 -0500

    Fix Docker build and add Web UI

--- midi-generator/app/main.py
+++ midi-generator/app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 import shutil
 import os
 from app.core import process_audio
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+@app.get("/")
+async def read_index():
+    return FileResponse('app/static/index.html')
 
 @app.post("/process")
 async def process_endpoint(file: UploadFile = File(...)):
